56727a1615ed9e9f8b97611553a14724ef9a77c5
Shadrackssien <shadrack6960@gmail.com>
2022-12-14 19:52:39 -0800

    Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_sign - print the sign of a number
+ * @n: the number os sign
+ * Return: 1 if number is greater than zero
+ * Return: 0 if number is zero
+ * Return: -1 if number is less than zero
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar_('-');
+		return (-1);
+	}
+}
