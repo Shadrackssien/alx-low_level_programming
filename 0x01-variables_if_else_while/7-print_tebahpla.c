ad4068c5b3f17335b080f2ffef00612142f8d7e4
Shadrackssien <shadrack6960@gmail.com>
2023-01-02 15:05:14 -0800

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - print the alphabets in reverse
+ * Return: Always 0
+ */
+int main(void)
+{
+	char le;
+
+	for (la = 'z'; le >= 'a'; le--)
+		putchar(le);
+
+	putchar ('\n');
+
+	return (0);
+}
