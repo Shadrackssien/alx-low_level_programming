9f889edf250cfb987248a98246046c5607c1ff71
Shadrackssien <shadrack6960@gmail.com>
2023-01-02 15:28:15 -0800

    Combination

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints all combination of single digits
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 49; n < 58; n++)
+	{
+		putchar(n);
+		if (n != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		putchar('\n');
+		return (0);
+	}
+}
